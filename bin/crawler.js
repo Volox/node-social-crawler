928386246ab3f2da57f5b2d7c98b4868f36e7b10
Riccardo Volonterio <voloxxx@gmail.com>
2015-06-24 12:23:29 +0200

    New version with better structure.

--- bin/crawler.js
+++ bin/crawler.js
@@ -1,27 +1,8 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
 'use strict';
-// Load system modules
 
-
-// Load modules
 var cli = require( '../lib/cli' );
 
-// Load my modules
-
-
-// Constant declaration
-
-
-// Module variables declaration
-
-
-// Module functions declaration
-
-
-// Module exports
-
-
-// Entry point
+process.title = 'Crawler';
 cli.argv( process.argv.slice( 2 ) );
-
 //  50 6F 77 65 72 65 64  62 79  56 6F 6C 6F 78
